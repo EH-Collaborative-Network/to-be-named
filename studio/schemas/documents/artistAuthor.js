281d71c93ca7bd3af4dc60513898cf722bfe69c3
Aarati Akkapeddi <aaratiakkapeddi@Aaratis-MBP.home>
2022-08-09 19:27:29 -0400

    hints and slug for artist

--- studio/schemas/documents/artistAuthor.js
+++ studio/schemas/documents/artistAuthor.js
@@ -13,6 +13,20 @@ export default {
       validation: Rule => Rule.required().error('name cannot be left blank')
     },
     {
+      name:'slug',
+      type: 'slug',
+      title: 'Slug (what should the link to this page look like)',
+      validation: Rule => Rule.required().error('slug cannot be left blank'),
+      options: {
+       source: 'name',
+       maxLength: 200, // will be ignored if slugify is set
+       slugify: input => input
+                            .toLowerCase()
+                            .replace(/\s+/g, '-')
+                            .slice(0, 200)
+     }
+   },
+    {
       name: 'image',
       title: 'Image',
       type: 'figure'
