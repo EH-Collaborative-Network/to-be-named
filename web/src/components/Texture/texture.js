21db79a55ed91e44db7a6b7a45b5ba6291829053
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2023-05-22 18:00:31 +0100

    mobile font size

--- web/src/components/Texture/texture.js
+++ web/src/components/Texture/texture.js
@@ -4,7 +4,7 @@ import sketch3 from "./sketch3";
 import sketch4 from "./sketch4";
 import sketch5 from "./sketch5";
 import sketchSix from "./sketchSix";
-import genesis from "./genesis.gif";
+import genesis from "./5.png";
 import {ReactP5Wrapper} from "react-p5-wrapper";
 import React from "react";
 import * as styles from "./texture.module.css";
